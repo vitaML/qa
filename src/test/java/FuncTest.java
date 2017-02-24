ba3b637d580d86bb3fadb6c86297d5afc6bb4a0a
Viktoriia_Martyniuk-Lototska <Viktoriia.Martyniuk-Lototska@thomsonreuters.com>
2017-02-24 14:57:27 +0200

    add

--- src/test/java/FuncTest.java
+++ src/test/java/FuncTest.java
@@ -1,12 +1,14 @@
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FuncTest {
     @Test
     public void testMethod() {
-        WebDriver driver1 = new FirefoxDriver();
-        driver1.get("http://www.google.com");
-        driver1.quit();
+        System.setProperty("webdriver.chrome.driver", getClass().getResource("/chromedriver.exe").getPath());
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.quit();
     }
 }
